02543f0cbe5156380ac35b7356f29e1e6ed533d5
slaugaus <slaugaus@gmail.com>
2023-05-19 13:45:05 -0600

    1.19.4 attempt

--- src/main/java/com/slaugaus/tinycokeredux/TinyCoke.java
+++ src/main/java/com/slaugaus/tinycokeredux/TinyCoke.java
@@ -3,27 +3,28 @@ package com.slaugaus.tinycokeredux;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+
+import com.slaugaus.tinycokeredux.init.ItemInit;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TinyCoke.MODID)
@@ -33,30 +34,22 @@ public class TinyCoke
     public static final String MODID = "tinycoke2";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-
-    public void TinyCoke()
+    public TinyCoke()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
+        
         // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
+        ItemInit.ITEMS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+//        // Register the item to a creative tab
+//        modEventBus.addListener(ItemInit.addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
@@ -65,6 +58,8 @@ public class TinyCoke
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
     }
+
+
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
@@ -75,7 +70,7 @@ public class TinyCoke
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
--- src/main/java/com/slaugaus/tinycokeredux/init/ItemInit.java
+++ src/main/java/com/slaugaus/tinycokeredux/init/ItemInit.java
@@ -1,4 +1,38 @@
-﻿package com.slaugaus.tinycokeredux.init;
+package com.slaugaus.tinycokeredux.init;
 
-public class ItemInit {
+import com.slaugaus.tinycokeredux.TinyCoke;
+import net.minecraft.world.item.*;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TinyCoke.MODID);
+    
+    public static final RegistryObject<Item> TINY = ITEMS.register("tinycoke",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> HALF = ITEMS.register("halfcoke",
+            () -> new Item(new Item.Properties()));
+
+//    private void addCreative(CreativeModeTabEvent.BuildContents event)
+//    {
+//        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
+//            event.accept();
+//    }
+    
+//    public static class ModCreativeTab extends CreativeModeTab {
+//        private ModCreativeTab(int index, String label) {
+//            super(index, label);
+//        }
+//        
+//        @Override
+//        public ItemStack makeIcon() {
+//            return new ItemStack(TINY.get());
+//        }
+//
+//        public static final ModCreativeTab instance = 
+//                new ModCreativeTab(CreativeModeTab.TABS.length, TinyCoke.MODID);
+//    }
 }
