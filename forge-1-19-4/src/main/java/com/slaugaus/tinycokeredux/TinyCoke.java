59e9b150e3219ec9c836548a1066f0e225dd8c50
slaugaus <slaugaus@gmail.com>
2023-08-08 22:59:32 -0700

    Base burn times on coke item instead of hardcoding

--- forge-1-19-4/src/main/java/com/slaugaus/tinycokeredux/TinyCoke.java
+++ forge-1-19-4/src/main/java/com/slaugaus/tinycokeredux/TinyCoke.java
@@ -1,8 +1,13 @@
 package com.slaugaus.tinycokeredux;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
@@ -10,6 +15,9 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.GameData;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -51,13 +59,20 @@ public class TinyCoke
         @SubscribeEvent
         public static void setBurnTimes(FurnaceFuelBurnTimeEvent event) {
             Item item = event.getItemStack().getItem();
-            // Tiny Coke smelts 1 item
+            // Get the burn time of coal coke, in case it isn't 3200
+            // TODO: i think this crashes
+            @Nullable Item cokeItem = ForgeRegistries.ITEMS.getValue(new ResourceLocation("immersiveengineering", "coal_coke"));
+            int cokeBurnTime;
+            if (cokeItem != null)
+                cokeBurnTime = ForgeHooks.getBurnTime(new ItemStack(cokeItem), RecipeType.SMELTING);
+            else cokeBurnTime = 3200;  // Fallback
+            // Tiny Coke smelts 1 item (1/16th of cBT)
             if (item == ModItems.TINY.get()) {
-                event.setBurnTime(200);
+                event.setBurnTime(cokeBurnTime / 16);
             }
-            // Half Coke smelts 8 items (same as normal coal)
+            // Half Coke smelts 8 items (1/2 of cBT; should be same as normal coal)
             else if (item == ModItems.HALF.get()) {
-                event.setBurnTime(1600);
+                event.setBurnTime(cokeBurnTime / 2);
             }
         }
     }
